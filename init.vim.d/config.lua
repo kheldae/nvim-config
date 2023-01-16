639cc37ffda569cae36c22583f50df8704d6ed3a
TheSola10 <me@thesola.io>
2023-01-16 14:50:23 +0100

    Added Nix language server

--- init.vim.d/config.lua
+++ init.vim.d/config.lua
@@ -104,6 +104,8 @@ lsp_with_coq(lsp.ocamllsp,      { cmd = nixsh("ocamlPackages.ocaml-lsp", "ocamll
 lsp_with_coq(lsp.vimls,         { cmd = nixsh("nodePackages.vim-language-server", "vim-language-server --stdio") })
 -- PureScript
 lsp_with_coq(lsp.purescriptls,  { cmd = nixsh("nodePackages.purescript-language-server", "purescript-language-server --stdio") })
+-- Nix
+lsp_with_coq(lsp.nil_ls,        { cmd = nixsh("nil", "nil") })
 
 
 vim.lsp.handlers['textDocument/codeAction']     = require'lsputil.codeAction'.code_action_handler
