8468683fc10915b24efdbe81cd7939d652154537
TheSola10 <me@thesola.io>
2023-03-20 16:22:37 +0100

    Added LSP DAP for C/C++/Rust

--- init.vim.d/config.lua
+++ init.vim.d/config.lua
@@ -170,8 +170,22 @@ coqx {
 }
 
 -- Debug Adapter Protocol
+dap.adapters.lldb = { type = 'executable', command = '/usr/bin/lldb-vscode', name = 'lldb' }
 
-
+dap.configurations.cpp = {
+    { name = 'Launch',
+      type = 'lldb',
+      request = 'launch',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = 'false',
+      args = {}
+    }
+}
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
 dap.configurations.java = {
     { type = 'java';
       request = 'attach';
