b59e52aa299f7c6c7416c847db08670cd134e308
TheSola10 <me@thesola.io>
2023-10-09 11:26:35 +0200

    Dropped explicit mention of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 { description = "Dependency locks for my Neovim config";
 
   inputs."nixpkgs".url = github:NixOS/nixpkgs;
-  inputs."flake-utils".url = github:numtide/flake-utils;
 
   outputs = { self, nixpkgs, flake-utils, ... }:
   flake-utils.lib.eachDefaultSystem
