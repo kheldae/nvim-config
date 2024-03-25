2c94e866854c6249c330b028696c0623560359d0
kheldae <taho.gougeon@gmail.com>
2024-03-25 17:40:53 +0100

    added 73-cmdline

--- init.vim.d/73-cmdline.lua
+++ init.vim.d/73-cmdline.lua
@@ -1,6 +1,22 @@
 --
--- Extended configuration of Folke's Noice plugin.
+-- Extended configuration of Folke's Noice plugin. This will require yet
+-- another color dictionnary.
 --
+
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#292b2f',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  yellow = '#e6db74',
+  grey   = '#777777',
+  silver = '#a0a0a0',
+  -- Custom colors
+  sand   = '#918154',
+  rsand  = '#875f5f',
+  darker = '#303030',
+}
 
 -- Noice section
 require'noice'.setup {
